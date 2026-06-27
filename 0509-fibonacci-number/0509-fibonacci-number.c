e256084915e57feacf65adc48c8d0f513c37c35d
Priyam Rasikbhai Kakadiya <priyamkakadiya1503@gmail.com>
2026-06-27 09:16:07 +0530

    Time: 0 ms (100%), Space: 7.6 MB (100%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.c
@@ -0,0 +1,17 @@
+int fib(int n) {
+
+
+    int a = 0; 
+    int b = 1; 
+    int c; 
+
+    if (n == 0) return 0; 
+    if (n == 1) return 1; 
+    
+    for (int i = 1; i < n; i++) {
+        c = a + b; 
+        a = b;    
+        b = c;     
+    }
+    return c;
+}
